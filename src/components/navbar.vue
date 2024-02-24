dff3d7fe9f2bb38adde1a6ce322edb59608db93f
Honglek <160992141+Honglek@users.noreply.github.com>
2024-02-24 14:43:53 +0700

    Update navbar.vue

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -17,7 +17,20 @@
               class="cursor-pointer mx-2 md:hidden block p-2 navbar-toggle"
               @click="toggleMenu($event)"
             >
-              <ion-icon>|||</ion-icon>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5 sm:w-6 sm:h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+                />
+              </svg>
             </span>
           </div>
         </div>
